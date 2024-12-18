575be19a088f2762ab3c3d70c10aa3533bc4d9cc
Bruno César <bcgmeireles@gmail.com>
2024-12-17 22:08:31 -0300

    feat: implements some basic structures

--- internal/model/product.go
+++ internal/model/product.go
@@ -1,11 +1,13 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Product struct {
-	ID          string
-	Name        string
-	Description string
-	Image       string
-	Price       float32
-	Category    string // TODO: Will be Category type
-	IsOnSale    bool   // When the product is an offer
+	ID          primitive.ObjectID `json:"id"`
+	Name        string             `json:"name"`
+	Description string             `json:"description"`
+	Image       string             `json:"image"`
+	Price       float32            `json:"price"`
+	Category    string             `json:"category"`  // TODO: Will be Category type
+	IsOnOffer   bool               `json:"isOnOffer"` // When the product is on offer
 }
