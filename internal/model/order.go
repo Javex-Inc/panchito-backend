c46946ba606edec570525858117565a6379d4359
Bruno César <bcgmeireles@gmail.com>
2024-12-17 23:59:48 -0300

    feat: implements complete payment and order struct

--- internal/model/order.go
+++ internal/model/order.go
@@ -9,13 +9,11 @@ import (
 type Order struct {
 	ID         primitive.ObjectID `json:"id"`
 	Number     int                `json:"number"`
-	Client     Client             `json:"client"`
+	ClientID   primitive.ObjectID `json:"clientId"`
 	Status     Status             `json:"status"`
 	IsDelivery bool               `json:"isDelivery"`
 	IsTakeaway bool               `json:"isTakeaway"`
 	Products   []Product          `json:"products"`
 	Payment    Payment            `json:"payment"`
-	Discount   float32            `json:"discount"`
-	Total      float32            `json:"total"`
 	Timestamp  time.Time          `json:"timestamp"`
 }
