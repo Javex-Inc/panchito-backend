6727793905b410851605473bd0f4b9f49befdc40
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-12-18 00:39:19 -0300

    fix: removed the underscore in ClientID

--- internal/model/order.go
+++ internal/model/order.go
@@ -9,7 +9,7 @@ import (
 type Order struct {
 	ID         primitive.ObjectID `json:"id" bson:"_id"`
 	Number     int                `json:"number" bson:"number"`
-	ClientID   primitive.ObjectID `json:"client_id" bson:"_client_id"` // its with "_" before!
+	ClientID   primitive.ObjectID `json:"client_id" bson:"client_id"`
 	Status     Status             `json:"status" bson:"status"`
 	IsDelivery bool               `json:"is_delivery" bson:"is_delivery"`
 	IsTakeaway bool               `json:"is_takeaway" bson:"is_takeaway"`
