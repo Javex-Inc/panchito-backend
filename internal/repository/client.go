5305b45f914c89b3a3d3c84a7ed02b851ea1edc9
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-12-23 01:04:57 -0300

    feat: created client repository

---
+++ internal/repository/client.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Javex-Inc/panchito-backend/internal/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ClientRepository struct {
+	collection *mongo.Collection
+}
+
+func NewClientRepository(client *mongo.Client, dbName string, collection string) *ClientRepository {
+	return &ClientRepository{
+		collection: client.Database(dbName).Collection(collection),
+	}
+}
+
+func (c *ClientRepository) InsertClient(client *model.Client) error {
+	_, err := c.collection.InsertOne(context.Background(), client)
+	if err != nil {
+		fmt.Errorf("error trying to insert user into the collection: %w", err)
+	}
+
+	return nil
+}
