0bcd6790d649e9916d113de26a59cf77241fab55
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-12-23 01:53:30 -0300

    feat: implements createClient route

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,16 +3,17 @@ package service
 import (
 	"os"
 
+	"github.com/Javex-Inc/panchito-backend/internal/handler"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func StartServer() {
+func StartServer(client *mongo.Client) {
 	app := fiber.New()
 	port := os.Getenv("SERVER_PORT")
+	ch := handler.NewClientHandler(client)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world!")
-	})
+	app.Post("/register", ch.CreateClient)
 
 	app.Listen(":" + port)
 }
