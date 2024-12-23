b3596324110cb8c122870b0b45d5505053eccbac
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-12-23 01:55:22 -0300

    refactor: changed zipCode json structure

--- internal/model/address.go
+++ internal/model/address.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Address struct {
 	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	ZipCode         string             `json:"zipCode" bson:"zipcode"`
+	ZipCode         string             `json:"zip_code" bson:"zip_code"`
 	Block           string             `json:"block" bson:"block"`
 	ResidentialUnit string             `json:"residential_unit" bson:"residential_unit"`
 	Number          string             `json:"number" bson:"number"`
