26d9e8a11d7a499d4e99deb069b37f91f3e7a54c
Bruno <bcgmeireles@gmail.com>
2025-01-07 10:49:28 -0300

    feat: implements get all products function

--- internal/repository/product.go
+++ internal/repository/product.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/Javex-Inc/panchito-backend/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -22,8 +23,30 @@ func (p *ProductRepository) InsertProduct(product *model.Product) error {
 	_, err := p.collection.InsertOne(context.Background(), product)
 
 	if err != nil {
-		fmt.Errorf("error trying to insert product: %w", err)
+		return fmt.Errorf("error trying to insert product: %w", err)
 	}
 
 	return nil
+}
+
+func (p *ProductRepository) GetAllProducts() ([]model.Product, error) {
+	var products []model.Product
+
+	cur, err := p.collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to find all products: %w", err)
+	}
+
+	for cur.Next(context.Background()) {
+		var product model.Product
+
+		err := cur.Decode(&product)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode product: %w", err)
+		}
+
+		products = append(products, product)
+	}
+
+	return products, nil
 }
