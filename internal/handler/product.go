fd50d7d770de98d8461343b7e638cc4d070b9fcc
Bruno César <bcgmeireles@gmail.com>
2025-01-08 16:09:02 -0300

    feat: implements cloudinary library to storage images in cloud

--- internal/handler/product.go
+++ internal/handler/product.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"context"
+	"os"
+	"strconv"
+
 	"github.com/Javex-Inc/panchito-backend/internal/factory"
-	"github.com/Javex-Inc/panchito-backend/internal/model"
 	"github.com/Javex-Inc/panchito-backend/internal/repository"
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -33,16 +38,43 @@ func (ph *ProductHandler) GetAllProducts(c *fiber.Ctx) error {
 }
 
 func (ph *ProductHandler) CreateProduct(c *fiber.Ctx) error {
-	var productBody model.Product
+	cloudName := os.Getenv("CLOUD_NAME")
+	apiKey := os.Getenv("API_KEY")
+	apiSecret := os.Getenv("API_SECRET")
 
-	err := c.BodyParser(&productBody)
+	form, err := c.MultipartForm()
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"message": "internal server error",
 		})
 	}
 
-	product := ph.factory.CreateProduct(productBody.Name, productBody.Description, productBody.Image, productBody.Category, productBody.Price, productBody.IsOnOffer)
+	name := form.Value["name"][0]
+	description := form.Value["description"][0]
+	image := form.File["image"][0]
+	price, _ := strconv.ParseFloat(form.Value["price"][0], 64)
+	category := form.Value["category"][0]
+	isOnOffer, _ := strconv.ParseBool(form.Value["is_on_offer"][0])
+
+	cloud, err := cloudinary.NewFromParams(cloudName, apiKey, apiSecret)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "internal server error in cloud connection",
+		})
+	}
+
+	uploadParams := uploader.UploadParams{
+		Folder: "products",
+	}
+
+	uploadResult, err := cloud.Upload.Upload(context.Background(), image, uploadParams)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "internal server error in upload result",
+		})
+	}
+
+	product := ph.factory.CreateProduct(name, description, uploadResult.SecureURL, category, price, isOnOffer)
 	err = ph.repository.InsertProduct(product)
 
 	if err != nil {
@@ -53,5 +85,8 @@ func (ph *ProductHandler) CreateProduct(c *fiber.Ctx) error {
 
 	return c.Status(201).JSON(fiber.Map{
 		"message": "success",
+		"product": product,
 	})
 }
+
+// func (ph *ProductHandler) uploadPhoto
